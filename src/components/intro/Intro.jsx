63e869d42235140d9604127459e6401cc7b33276
Tran Dinh Hieu <davidtranit110@gmail.com>
2024-01-08 11:43:44 +0700

    Portfolio Page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,10 +1,37 @@
 // import React from "react";
+import { useEffect, useRef } from "react";
 import "./intro.scss";
+import { init } from "ityped";
 
 export default function Intro() {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backSpeed: 60,
+      backDelay: 1500,
+      strings: ["FullStack", "Front-End", "Back-End"],
+    });
+  }, []);
   return (
     <div className="intro" id="intro">
-      Intro
+      <div className="left">
+        <div className="imageContainer">
+          <img src="assets/david.png" alt="David.png" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi there, I'm</h2>
+          <h1>Trần Đình Hiệu</h1>
+          <h3>
+            A Software Developer <span ref={textRef}></span>
+          </h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/down.png" alt="down.img" />
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,7 +4,46 @@ import "./portfolio.scss";
 export default function Portfolio() {
   return (
     <div className="portfolio" id="portfolio">
-      <h2>Portfolio</h2>
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Web App</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Content</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img
+            src="https://private-user-images.githubusercontent.com/115960016/288109282-11580bb6-22cf-4563-aeb8-991f222f518d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ2ODM1NjIsIm5iZiI6MTcwNDY4MzI2MiwicGF0aCI6Ii8xMTU5NjAwMTYvMjg4MTA5MjgyLTExNTgwYmI2LTIyY2YtNDU2My1hZWI4LTk5MWYyMjJmNTE4ZC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMTA4JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDEwOFQwMzA3NDJaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02ZTM3N2UwY2M4ZjRlOTcxZmU0ZTUwNzUwNzJjOWYyMTdhYjg1YjAyNDEzMDEzZWU5M2MxYjUyZGFiN2I0ZjZkJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.VFfpydgR8oimUzMgy0MHV4RhEnbGOJZOktJb2F96vnM"
+            alt=""
+          />
+          <h3>JSP-SERVLET</h3>
+        </div>
+        <div className="item">
+          <img src="assets/node.jpg" alt="" />
+          <h3>NODE JS</h3>
+        </div>
+        <div className="item">
+          <img src="assets/drum.jpg" alt="" />
+          <h3>Javascript</h3>
+        </div>
+        <div className="item">
+          <img src="assets/calculator.jpg" alt="" />
+          <h3>Javascript</h3>
+        </div>
+        <div className="item">
+          <img src="assets/simon.jpg" alt="" />
+          <h3>Javascript</h3>
+        </div>
+        <div className="item">
+          <img
+            src="https://private-user-images.githubusercontent.com/115960016/288109282-11580bb6-22cf-4563-aeb8-991f222f518d.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MDQ2ODM1NjIsIm5iZiI6MTcwNDY4MzI2MiwicGF0aCI6Ii8xMTU5NjAwMTYvMjg4MTA5MjgyLTExNTgwYmI2LTIyY2YtNDU2My1hZWI4LTk5MWYyMjJmNTE4ZC5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwMTA4JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDEwOFQwMzA3NDJaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT02ZTM3N2UwY2M4ZjRlOTcxZmU0ZTUwNzUwNzJjOWYyMTdhYjg1YjAyNDEzMDEzZWU5M2MxYjUyZGFiN2I0ZjZkJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.VFfpydgR8oimUzMgy0MHV4RhEnbGOJZOktJb2F96vnM"
+            alt=""
+          />
+          <h3>Sparkling Stones</h3>
+        </div>
+      </div>
     </div>
   );
 }
